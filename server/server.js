b1ba418e07f02521a69f97dde829c7396bc072a2
SakshamSharma067 <sharma.saksham067@gmail.com>
2025-05-16 02:12:34 +0530

    updated origin

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', 'https://s-mart-groceries-epym.vercel.app']
+const allowedOrigins = ['http://localhost:5173', 'https://s-mart-groceries-upzy.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
